86d2de2a84cf057da25996a71d59925a6aba5c73
vsanm01 <155801539+vsanm01@users.noreply.github.com>
2025-10-08 09:37:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 ```javascript
 // Configuration file for the e-commerce system
 const ECOM_CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzBEK-wd9UcIxf_vEk1otZ43fBl8ZZ6XEDygHuflJNigJQ52db7Hprsaiim5CqYQlD27g/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxZssNUTXM1Or7grmlYUw12U_jcQFgynxST-0brL4LtOZmZkh6ADiG0Zm3yMtYoXMzCoA/exec',
     WHATSAPP_NUMBER: '1234567890', // Change this
     DELIVERY_CHARGE: 50,
     CURRENCY_SYMBOL: '₹',
@@ -21,5 +21,6 @@ window.ECOM_STATE = {
     deliveryType: 'delivery',
     allCategories: []
 };
+
 
 
