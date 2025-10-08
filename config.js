7c50d8489899384ce20fdee050b6e9371c7600ed
vsanm01 <155801539+vsanm01@users.noreply.github.com>
2025-10-08 11:14:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,19 +25,3 @@
     }, 4000);
   });
 })();
-
-(function() {
-    window.ECOM_CONFIG = {
-        SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxZssNUTXM1Or7grmlYUw12U_jcQFgynxST-0brL4LtOZmZkh6ADiG0Zm3yMtYoXMzCoA/exec',
-        WHATSAPP_NUMBER: '1234567890'
-    };
-
-    window.ECOM_STATE = {
-        products: [],
-        cart: [],
-        currentCategory: 'all'
-    };
-
-    console.log('✅ config.js loaded and executed');
-})();
-
